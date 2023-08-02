c412bb5711455884c8f3af63a6b149856336d0f7
jesus <pereirajesus35@gmail.com>
2023-08-01 23:03:41 -0400

    update about us

--- components/About/About.vue
+++ components/About/About.vue
@@ -1,20 +1,32 @@
 <template>
   <div class="root">
-    <v-container class="fixed-width">
+    
+    <v-container fluid class="fixed-width">
+      
       <v-row>
-        <v-col cols="12" md="5" class="pa-6">
-          <div class="text-center">
-            <title-deco :text="$t('medicalLanding.about_title')" :bg="photo"/>
+       
+        <v-col cols="12"  >
+        
+          <div class="containerdiv">
+            
+            <img :src=integral alt="" class="img2">
+            <v-card class="curved-right" >
+            
+        <v-card-text>
+          <!-- Aquí coloca tu texto u otros elementos dentro de la card -->
+          <title-deco text="Quienes somos?" :bg="photo"/>
+          <p class="title-default use-text-subtitle">
+            <a class="text">"Urpe Integral Services</a> es como tu amigo confiable que te guía a través de los trámites"
+          </p>
+        
+          <p class="title-default use-text-subtitle">
+          "Desde inmigración, impuestos, reparación de crédito y bienes raíces en los Estados Unidos.
+            Nos adaptamos a tus necesidades, ofreciéndote consejos y soluciones personalizadas para ayudarte a alcanzar tu estabilidad.
+            En Urpe, estamos a tu lado en cada paso del camino, haciendo tu vida más fácil y próspera en tu nuevo hogar."
+          </p>
+        </v-card-text>
+      </v-card>
           </div>
-        </v-col>
-        <v-col cols="12" md="7" class="pa-6">
-          <h4 class="title-default use-text-subtitle">
-            {{ $t('medicalLanding.about_subtitle') }}
-          </h4>
-          <counter />
-          <blockquote>
-            {{ $t('medicalLanding.about_quote') }}
-          </blockquote>
         </v-col>
       </v-row>
     </v-container>
@@ -28,17 +40,20 @@
 
 <script>
 import imgAPI from '~/static/images/imgAPI'
-import Counter from '../Counter'
+
 import TitleDeco from '../Title/WithDecoration'
+import somos from '../../static/images/urpe/somos.jpg'
+import integral from '../../static/images/urpe/integral2.png'
 
 export default {
   data() {
     return {
-      photo: imgAPI.medical[1]
+      photo: imgAPI.medical[1],
+      somos,
+      integral
     }
   },
   components: {
-    Counter,
     TitleDeco
   },
   computed: {
